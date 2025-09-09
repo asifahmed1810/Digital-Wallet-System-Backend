938baaab0fd9ae3805f3490b15edfd40a6523217
asifahmed1810 <asifahmed18oct@gmail.com>
2025-09-09 15:44:12 +0600

    Added user folder and Route folder

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,33 @@
+import { Types } from "mongoose"
+
+export enum Role{
+    ADMIN="ADMIN",
+    USER="USER",
+    AGENT="AGENT"
+}
+
+
+export enum IsActive{
+    ACTIVE="ACTIVE",
+    INACTIVE="INACTIVE",
+    BLOCKED="BLOCKED"
+}
+
+export interface IAuthProvider{
+    provider:string,
+    providerId:string
+}
+
+
+export interface IUser{
+    name:string,
+    email:string,
+    password ?:string,
+    role ?:Role,
+    phone ?:string,
+    picture ?:string,
+    isActive ?: IsActive,
+    isApproved ?:string,
+    auths:IAuthProvider[],
+    wallet?:Types.ObjectId[]
+}
