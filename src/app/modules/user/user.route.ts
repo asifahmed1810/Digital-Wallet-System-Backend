5ee8f539981c2f651f255416a642376496b63e4a
asifahmed1810 <asifahmed18oct@gmail.com>
2025-09-10 15:06:32 +0600

    Added password hashing

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
-const router=Router();
+const router = Router();
 
-router.post("/register",UserControllers.createUser)
-router.get("/all-users", UserControllers.getAllUsers)
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserControllers.createUser
+);
 
-export const userRoutes=router;
+
+
+router.get("/all-users", UserControllers.getAllUsers);
+
+
+
+export const userRoutes = router;
